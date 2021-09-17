fa0ba294619c3997036c375242f6299534eac67e
Bryan Counts <b.counts@gmail.com>
2021-09-17 15:35:34 -0400

    bgc - added script to retrieve resource list

---
+++ Powershell/get-gcpComputeAssetList.ps1
@@ -0,0 +1,23 @@
+<#
+    This script retrieves compute engine assets via the Asset Manager API. Requirments to use this script include:
+    1.) Account with Cloud Asset Viewer role assigned at the Organization level
+    2.) Gcloud SDK installed and initiated with authentication to your organization. 
+
+    Asset types can be viewed here: 
+    https://cloud.google.com/asset-inventory/docs/supported-asset-types
+
+    Example:
+
+    ./get-gcpComputeAssetList.ps1 -orgid <organization ID> -resource <compute resource, i.e firewall or interconnect>
+#>
+
+param (
+    $orgid,
+    $resource
+)
+$Projects = gcloud projects list --format="json"
+$ProjectList = $Projects | ConvertFrom-Json
+
+$resources = gcloud asset list --organization $orgid --filter=assetType:compute.googleapis.com/$resource --format="json" 
+$resourceList = $resources | ConvertFrom-Json
+return $resourceList
